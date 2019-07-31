f560e18b5b5a992b2efd88d4dcbd2b24005550a4
wilgustavo <wilgustavo@gmail.com>
2019-07-31 19:57:25 -0300

    Se agrega página de detalle
    Se crea un nuevo método en la clase `HomeController` para
    obtener un detalle de una película y se crea la página JSP.

--- src/main/java/com/ma/app/controller/HomeController.java
+++ src/main/java/com/ma/app/controller/HomeController.java
@@ -1,15 +1,30 @@
 package com.ma.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String goHome() {
 		return "home";
 	}
 
+
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+	public String mostrarDetalle(Model model) {
+		String tituloPelicula = "Rapido y furioso";
+		int duracion = 136;
+		double precioEntrada = 50.0;
+
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		return "detalle";
+	}
+
 }
+
---
+++ webapp/WEB-INF/views/detalle.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Detalle de la pel&iacute;cula</title>
+</head>
+<body>
+	<h1>Título de la película ${titulo}</h1>
+	<h2>Duración ${duracion} min.</h2>
+	<h2>Precio ${precio}</h2>
+</body>
+</html>
