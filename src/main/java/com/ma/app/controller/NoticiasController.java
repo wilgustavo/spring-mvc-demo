85be485fb9f1a7ff60a557e7e028c47263942fae
wilgustavo <wilgustavo@gmail.com>
2019-08-04 22:12:49 -0300

    Formulario para crear noticia
    Se agrega un controlador `NoticiasController`
    que maneja las acciones de consultar y guardar una
    noticia.
    
    Se crea una vista formNoticia.jsp.
    
    Se incluye un editor de texto utilizando la libreria
    TinyMCE.

---
+++ src/main/java/com/ma/app/controller/NoticiasController.java
@@ -0,0 +1,31 @@
+package com.ma.app.controller;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/noticias")
+public class NoticiasController {
+
+    private static final Logger logger = Logger.getLogger(HomeController.class.getName());
+
+    @GetMapping("/create")
+    public String crear() {
+        return "noticias/formNoticia";
+    }
+
+    @PostMapping("/save")
+    public String guardar(@RequestParam("titulo") String titulo, @RequestParam("estatus") String estatus,
+            @RequestParam("detalle") String detalle) {
+
+        logger.log(Level.INFO, "Titulo: {0}, estatus {1}, detalle: {2}", new Object[] { titulo, estatus, detalle });
+        return "noticias/formNoticia";
+    }
+
+}
