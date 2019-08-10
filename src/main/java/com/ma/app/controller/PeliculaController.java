3b756e0a2ea6eafdbc50a275b6b45dbe2b422935
wilgustavo <wilgustavo@gmail.com>
2019-08-10 20:47:10 -0300

    Listado de peliculas
    Se crea la vista de listado de peliculas para el ABM.
    El listado se renderiza utilizando `listPeliculas.jsp`.
    
    Se crea un metodo en el controlador para obtener el
    listado de películas.

--- src/main/java/com/ma/app/controller/PeliculaController.java
+++ src/main/java/com/ma/app/controller/PeliculaController.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,12 @@ public class PeliculaController {
     public void initBinder(WebDataBinder binder) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, false));
+    }
+
+    @GetMapping("/index")
+    public String mostrarIndex(Model model) {
+        model.addAttribute("peliculas", peliculaService.buscarTodas());
+        return "peliculas/listPeliculas";
     }
 
     @GetMapping("/create")
