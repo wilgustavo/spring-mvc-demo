55922c07e194909547e9fc7e54cc28af7f995f46
wilgustavo <wilgustavo@gmail.com>
2019-08-25 09:46:32 -0300

    Incluir servidor Jetty
    Se modifica la configuración del proyecto
    para ejectuarlo en un servidor Jetty embebido
    
    Se incluye plugin de Maven para ejecutar un servidor
    Jetty.
    
    Como hay problemas con la subida de archivos estándar con
    el servidor Jetty, se opta por la librería CommonsFileUpload.
    
    Se cambia la ubicación del directorio webapp para adecuar la
    estructura de directorios al estandar de Maven.

--- src/main/java/com/ma/app/util/ImagenUtil.java
+++ src/main/java/com/ma/app/util/ImagenUtil.java
@@ -3,6 +3,8 @@ package com.ma.app.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,8 +12,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ImagenUtil {
 
-    private static final int SIZE_RANDOM = 8;
+    private static final Logger logger = Logger.getLogger(ImagenUtil.class.getName());
+
+    private static final int SIZE_RANDOM = 7;
     private static final String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String NOMBRE_PREFIJO = "x";
     private static final String RESOURCES_IMAGES = "/resources/images/";
     private static Random random = new Random();
 
@@ -19,14 +24,22 @@ public class ImagenUtil {
 
     public static String guardarImagen(MultipartFile multipartFile, HttpServletRequest request) {
         String nombreOriginal = getNombre(multipartFile);
-        String rutaFinal = request.getServletContext().getRealPath(RESOURCES_IMAGES);
+        String rutaFinal = getRutaFinal(request);
         try {
             File imageFile = new File(rutaFinal + nombreOriginal);
             multipartFile.transferTo(imageFile);
             return nombreOriginal;
         } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
             return null;
         }
+    }
+
+    private static String getRutaFinal(HttpServletRequest request) {
+        String rutaFinal = request.getServletContext().getRealPath(RESOURCES_IMAGES);
+        rutaFinal = rutaFinal.endsWith(File.separator) ? rutaFinal :
+                    rutaFinal.concat(File.separator);
+        return rutaFinal;
     }
 
     private static String getNombre(MultipartFile multipartFile) {
@@ -35,6 +48,7 @@ public class ImagenUtil {
 
     private static String randomAlphaNumeric(int count) {
         StringBuilder sb = new StringBuilder();
+        sb.append(NOMBRE_PREFIJO);
         while (count-- > 0) {
             sb.append(CARACTERES.charAt(random.nextInt(CARACTERES.length())));
         }
