ae891ed9bddeb6353f14bd8132637765e3c912df
wilgustavo <wilgustavo@gmail.com>
2019-08-31 21:42:00 -0300

    Resolver problema de N+1 al consultar
    Se utiliza un EntityGraph para resolver el problema
    de N+1 con Hibernate.

--- src/main/java/com/ma/app/repository/PeliculaRepository.java
+++ src/main/java/com/ma/app/repository/PeliculaRepository.java
@@ -1,8 +1,14 @@
 package com.ma.app.repository;
 
 import com.ma.app.model.Pelicula;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PeliculaRepository extends JpaRepository<Pelicula, Integer> {
 
+    @Override
+    @EntityGraph(attributePaths = {"detalle", "horarios"})
+    List<Pelicula> findAll();
 }
